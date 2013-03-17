438ea15a6f4913f02b996ca4e86ce03efc709203
wagnersousa <wagner.sousa.f@gmail.com>
2013-03-17 14:21:35 -0300

    bufo.

---
+++ src/Teste.java
@@ -0,0 +1,45 @@
+public class Teste {
+	public static void main(String[] args) {
+		
+		int[] a = new int[10];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = (int)Math.random() * 10;
+		}
+		
+		System.out.println(a);
+		cocktailSort(a);
+		System.out.println(a);
+		
+	}
+	
+	public static int[] cocktailSort(int[] vetor) {
+		boolean swap = true;
+		int i = 0;
+		int j = vetor.length - 1;
+		while (i < j && swap) {
+			swap = false;
+			for (int k = i; k < j; k++) {
+				if (vetor[k] > vetor[k + 1]) {
+					int temp = vetor[k];
+					vetor[k] = vetor[k + 1];
+					vetor[k + 1] = temp;
+					swap = true;
+				}
+			}
+			j--;
+			if (swap) {
+				swap = false;
+				for (int k = j; k > i; k--) {
+					if (vetor[k] < vetor[k - 1]) {
+						int temp = vetor[k];
+						vetor[k] = vetor[k - 1];
+						vetor[k - 1] = temp;
+						swap = true;
+					}
+				}
+			}
+			i++;
+		}
+		return vetor;
+	}
+}
--- src/ordena.java
+++ src/ordena.java
@@ -6,30 +6,64 @@ import java.io.IOException;
 public class ordena {
 	public static void main(String[] args) throws IOException {
 		int tamanhoDoVetor = Integer.parseInt(args[0]);
-		int tipoDeOrdenacao = Integer.parseInt(args[1]);		
+		int tipoDeOrdenacao = Integer.parseInt(args[1]);
 		int[] vetor = new int[tamanhoDoVetor];
-		
-		geraLista(vetor, tipoDeOrdenacao);
-		
+
+		String ordenacao = geraLista(vetor, tipoDeOrdenacao);
+
 		int[] vetorCopia = vetor.clone();
+		System.out.println("Insercao (" + vetorCopia.length + " - " + ordenacao + ")");
 		long tempo = System.nanoTime();
 		insercao(vetorCopia);
-		System.out.println("Insercao: " + (System.nanoTime() - tempo)/1000000.0 + " ms");
-		
+		System.out.println("Tempo: " + (System.nanoTime() - tempo) / 1000000.0 + " ms");
+
 		vetorCopia = vetor.clone();
+		System.out.println("Selecao (" + vetorCopia.length + " - " + ordenacao + ")");
 		tempo = System.nanoTime();
 		selecao(vetorCopia);
-		System.out.println("Selecao: " + (System.nanoTime() - tempo)/1000000.0 + " ms");
-		
+		System.out.println("Tempo: " + (System.nanoTime() - tempo) / 1000000.0 + " ms");
+
 		vetorCopia = vetor.clone();
+		System.out.println("ShellSort (" + vetorCopia.length + " - " + ordenacao + ")");
 		tempo = System.nanoTime();
 		shellSort(vetorCopia);
-		System.out.println("ShellSort: " + (System.nanoTime() - tempo)/1000000.0 + " ms");
-		
+		System.out.println("Tempo: " + (System.nanoTime() - tempo) / 1000000.0 + " ms");
+
 		vetorCopia = vetor.clone();
+		System.out.println("MergeSort (" + vetorCopia.length + " - " + ordenacao + ")");
 		tempo = System.nanoTime();
 		mergeSort(vetorCopia, 0, vetorCopia.length - 1);
-		System.out.println("MergeSort: " + (System.nanoTime() - tempo)/1000000.0 + " ms");
+		System.out.println("Tempo: " + (System.nanoTime() - tempo) / 1000000.0 + " ms");
+	}
+
+	public static void cocktailSort(int[] vetor) {
+		boolean swap = true;
+		int i = 0;
+		int j = vetor.length - 1;
+		while (i < j && swap) {
+			swap = false;
+			for (int k = i; k < j; k++) {
+				if (vetor[k] > vetor[k + 1]) {
+					int temp = vetor[k];
+					vetor[k] = vetor[k + 1];
+					vetor[k + 1] = temp;
+					swap = true;
+				}
+			}
+			j--;
+			if (swap) {
+				swap = false;
+				for (int k = j; k > i; k--) {
+					if (vetor[k] < vetor[k - 1]) {
+						int temp = vetor[k];
+						vetor[k] = vetor[k - 1];
+						vetor[k - 1] = temp;
+						swap = true;
+					}
+				}
+			}
+			i++;
+		}
 	}
 
 	public static void insercao(int[] vetor) {
@@ -43,7 +77,7 @@ public class ordena {
 				j--;
 			}
 			vetor[j + 1] = pivo;
-		}		
+		}
 	}
 
 	public static void selecao(int[] vetor) {
@@ -62,7 +96,7 @@ public class ordena {
 
 	public static void shellSort(int[] vetor) {
 		int hlistas = 1;
-		
+
 		while (hlistas < vetor.length)
 			hlistas = 3 * hlistas + 1;
 		do {
@@ -70,7 +104,7 @@ public class ordena {
 			for (int i = 0; i < vetor.length; i++) {
 				int pivo = vetor[i];
 				int j = i - hlistas;
-		
+
 				while (j >= 0 && vetor[j] > pivo) {
 					vetor[j + hlistas] = vetor[j];
 					j = j - hlistas;
@@ -79,114 +113,118 @@ public class ordena {
 			}
 		} while (hlistas > 1);
 	}
-	
+
 	public static void mergeSort(int[] vetor, int inicio, int fim) {
-        if (inicio < fim) {
-            int meio = (int)Math.floor((inicio + fim) / 2d);
-            if (inicio < meio)
-                mergeSort(vetor, inicio, meio);
-            if (meio + 1 < fim)
-                mergeSort(vetor, meio + 1, fim);
+		if (inicio < fim) {
+			int meio = (int) Math.floor((inicio + fim) / 2d);
+			if (inicio < meio)
+				mergeSort(vetor, inicio, meio);
+			if (meio + 1 < fim)
+				mergeSort(vetor, meio + 1, fim);
 
-            merge(vetor, inicio, meio, fim);
-        }
-    }
-	
-	private static void merge(int[] vetor, int inicio, int meio, int fim) {
-        int i = inicio, j = meio + 1, k = 0;
-        int vetorAux[] = new int[fim - inicio];
-        
-        while (i <= meio && j <= fim) {
-            if (vetor[i] <= vetor[j]) {
-                vetorAux[k] = vetor[i];
-                i++;
-            } else {
-                vetorAux[k] = vetor[j];
-                j++;
-            }
-            k++;
-        }
-        if (i <= meio) {
-            for (j = meio; j >= i; j--)
-                vetor[fim - meio + j] = vetor[j];
-        }
-        for (i = 0; i < k; i++)
-            vetor[inicio + i] = vetorAux[i];
-    }
-	
-	public static void quickSort(int[] vetor, int inicio, int fim) {
-        if (inicio < fim) {
-            int particao = particao(vetor, inicio, fim);
-            if (inicio < particao - 1)
-                quickSort(vetor, inicio, particao - 1);
-            else if (particao + 1 < fim)
-                quickSort(vetor, particao + 1, fim);
-        }
-    }
-
-    private static int particao(int[] vetor, int inicio, int fim) {
-        int pivo = vetor[inicio], i = inicio + 1, j = fim;
-        while (i <= j) {
-            while (i <= j && vetor[i] <= pivo)
-                i++;
-            while (vetor[j] > pivo)
-                j--;
-            if (i <= j) {
-                int swap = vetor[i];
-                vetor[i] = vetor[j];
-                vetor[j] = swap;
-                i++; j--;
-            }
-        }
-        int troca = vetor[inicio];
-        vetor[inicio] = vetor[j];
-        vetor[j] = troca;
-
-        return j;
-    }
-	
-	private static void geraLista(int vetor[], int opcao) {
-		if(opcao == 1) {
-			for (int i = 0; i < vetor.length; i++) {
-				vetor[i] = (int)(Math.random() * vetor.length) * 10; //gerando números aleatórios de 1 até o tamanho do vetor
-			}
-		} else if(opcao == 2) {
-			int j = 0;
-			for (int i = vetor.length - 1; i > 0; i--) { 
-				vetor[j] = i * 10; //gerando lista em ordem decrescente
-				j++;
-			}
-		} else {
-			for (int i = 0; i < vetor.length; i++) {
-				vetor[i] = i * 10; //gerando lista em ordem crescente
-			}
+			merge(vetor, inicio, meio, fim);
 		}
 	}
-	
+
+	private static void merge(int[] vetor, int inicio, int meio, int fim) {
+		int i = inicio, j = meio + 1, k = 0;
+		int vetorAux[] = new int[fim - inicio];
+
+		while (i <= meio && j <= fim) {
+			if (vetor[i] <= vetor[j]) {
+				vetorAux[k] = vetor[i];
+				i++;
+			} else {
+				vetorAux[k] = vetor[j];
+				j++;
+			}
+			k++;
+		}
+		if (i <= meio) {
+			for (j = meio; j >= i; j--)
+				vetor[fim - meio + j] = vetor[j];
+		}
+		for (i = 0; i < k; i++)
+			vetor[inicio + i] = vetorAux[i];
+	}
+
+	public static void quickSort(int[] vetor, int inicio, int fim) {
+		if (inicio < fim) {
+			int particao = particao(vetor, inicio, fim);
+			if (inicio < particao - 1)
+				quickSort(vetor, inicio, particao - 1);
+			else if (particao + 1 < fim)
+				quickSort(vetor, particao + 1, fim);
+		}
+	}
+
+	private static int particao(int[] vetor, int inicio, int fim) {
+		int pivo = vetor[inicio], i = inicio + 1, j = fim;
+		while (i <= j) {
+			while (i <= j && vetor[i] <= pivo)
+				i++;
+			while (vetor[j] > pivo)
+				j--;
+			if (i <= j) {
+				int swap = vetor[i];
+				vetor[i] = vetor[j];
+				vetor[j] = swap;
+				i++;
+				j--;
+			}
+		}
+		int troca = vetor[inicio];
+		vetor[inicio] = vetor[j];
+		vetor[j] = troca;
+
+		return j;
+	}
+
+	private static String geraLista(int vetor[], int opcao) {
+		if (opcao == 1) {
+			for (int i = 0; i < vetor.length; i++) {
+				vetor[i] = (int) (Math.random() * vetor.length) * 10; // gerando nï¿½meros aleatï¿½rios de 1 atï¿½ o tamanho do vetor
+			}
+			return "Aleatï¿½ria";
+		} else if (opcao == 2) {
+			int j = 0;
+			for (int i = vetor.length - 1; i > 0; i--) {
+				vetor[j] = i * 10; // gerando lista em ordem decrescente
+				j++;
+			}
+			return "Decrescente";
+		} else {
+			for (int i = 0; i < vetor.length; i++) {
+				vetor[i] = i * 10; // gerando lista em ordem crescente
+			}
+			return "Crescente";
+		}
+	}
+
 	private static void escreveVetorEmUmArquivo(String metodoDeOrdenacao, int[] vetorEmPossivelDesordem, int[] vetorOrdenado) throws IOException {
 		File saida = new File("saida.txt");
 		saida.createNewFile();
-		
+
 		FileWriter fileWriter = new FileWriter(saida);
-		
+
 		BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-		
+
 		bufferedWriter.write(metodoDeOrdenacao);
 		bufferedWriter.newLine();
 		bufferedWriter.write("Antes: ");
-		
+
 		for (int i = 0; i < vetorEmPossivelDesordem.length; i++) {
 			bufferedWriter.write(vetorEmPossivelDesordem[i] + " ");
-		}		
-		
+		}
+
 		bufferedWriter.newLine();
 		bufferedWriter.write("Depois: ");
-		
+
 		for (int i = 0; i < vetorOrdenado.length; i++) {
 			bufferedWriter.write(vetorOrdenado[i] + " ");
 		}
 		bufferedWriter.newLine();
-		
+
 		bufferedWriter.close();
 		fileWriter.close();
 	}
